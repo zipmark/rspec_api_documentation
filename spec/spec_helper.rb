6f17688134d6c08b24bb7b06e02fce88a7baf771
Sam Goldman <sam@smartlogicsolutions.com>
2011-10-20 16:48:26 -0400

    Require less for faster tests.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,8 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-
-Bundler.require :default, :test
-
-require 'active_support'
-require 'rails/railtie'
+require 'active_support/dependencies/autoload'
+require 'rspec_api_documentation'
 
 module Rails
   def self.root
     Pathname.new("tmp")
   end
 end
-
-require 'rspec_api_documentation'
