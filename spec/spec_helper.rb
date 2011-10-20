895828d002ef7877dfee52a17cbe3f953cafe1d2
Eric Oestrich <eric@oestrich.org>
2011-10-20 13:08:06 -0400

    more specific spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,15 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
-require 'rails'
+Bundler.require :default, :test
+
+require 'active_support'
+require 'rails/railtie'
+
+module Rails
+  def self.root
+    Pathname.new("tmp")
+  end
+end
+
 require 'rspec_api_documentation'
