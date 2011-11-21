11184b0eb9761adc30b4601eb40608420290b2da
Sam Goldman <samwgoldman@gmail.com>
2011-11-21 10:23:17 -0500

    Make TestServer rack-compatible

--- lib/rspec_api_documentation/test_server.rb
+++ lib/rspec_api_documentation/test_server.rb
@@ -1,5 +1,9 @@
 module RspecApiDocumentation
   class TestServer < Struct.new(:session)
-    delegate :example, :to => :session
+    delegate :example, :last_request, :last_response, :to => :session
+
+    def call(env)
+      return [200, {}, [""]]
+    end
   end
 end
--- spec/test_server_spec.rb
+++ spec/test_server_spec.rb
@@ -1,4 +1,6 @@
 require 'spec_helper'
+require 'webmock/rspec'
+require 'rack/test'
 
 describe RspecApiDocumentation::TestServer do
   let(:test_server) { described_class.new(self) }
@@ -7,4 +9,24 @@ describe RspecApiDocumentation::TestServer do
 
   its(:session) { should equal(self) }
   its(:example) { should equal(example) }
+
+  context "being called as a rack application" do
+    include Rack::Test::Methods
+
+    let(:app) { test_server }
+
+    before { post "/path" }
+
+    it "should expose the last request" do
+      test_server.last_request.should equal(last_request)
+    end
+
+    it "should expose the last response" do
+      test_server.last_response.should equal(last_response)
+    end
+
+    it "should always return 200" do
+      last_response.status.should eq(200)
+    end
+  end
 end
