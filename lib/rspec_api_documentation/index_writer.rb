011e5d555a3dcd833c4ca610b404fa48c2d6c3f2
Sam Goldman <samwgoldman@gmail.com>
2012-04-11 22:19:48 -0400

    Use activesupport Enumerable#group_by, which preserves order on 1.8.7.

--- lib/rspec_api_documentation/index_writer.rb
+++ lib/rspec_api_documentation/index_writer.rb
@@ -1,9 +1,11 @@
+require "active_support/core_ext/enumerable"
+
 module RspecApiDocumentation
   module IndexWriter
     def sections(examples, configuration)
       resources = examples.group_by(&:resource_name).inject([]) do |arr, (resource_name, examples)|
         ordered_examples = configuration.keep_source_order ? examples : examples.sort_by(&:description)
-        arr << { :resource_name => resource_name, :examples => ordered_examples }
+        arr.push(:resource_name => resource_name, :examples => ordered_examples)
       end
       configuration.keep_source_order ? resources : resources.sort_by { |resource| resource[:resource_name] }
     end
