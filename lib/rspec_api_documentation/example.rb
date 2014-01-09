e40931bff08e99af6470f42fb5986defd892af31
Benjie Gillam <benjie@jemjie.com>
2014-01-09 17:02:41 +0000

    Allow documenting multipart/form-data submissions

--- lib/rspec_api_documentation/example.rb
+++ lib/rspec_api_documentation/example.rb
@@ -43,7 +43,17 @@ module RspecApiDocumentation
     end
 
     def requests
-      metadata[:requests] || []
+      reqs = metadata[:requests] || []
+      reqs.each do |req|
+        if req[:request_headers]["Content-Type"].match /\Amultipart\/form-data/
+          i = req[:request_body].index /^Content-Disposition: form-data.* filename=\"/
+          i = req[:request_body].index "\r\n\r\n", i unless i.nil?
+          unless i.nil?
+            req[:request_body] = "#{req[:request_body][0..i+3]}...[truncated file data]..."
+          end
+        end
+      end
+      reqs
     end
   end
 end
