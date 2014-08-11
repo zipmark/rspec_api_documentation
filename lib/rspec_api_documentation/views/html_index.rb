102f4b397aa2f817fa64bdc513822e1f03fd00e0
Ty Rauber <tyrauber@mac.com>
2014-08-11 09:50:01 -0700

    feature/stylesheet :: A basic css stylesheet, with font and table styling, without the need for bootstrap.
    The stylesheet resides at lib/rspec_api_documentation/assets/stylesheets/rspec_api_documentation/styles.css, but may be overridden by creating a stylesheet in the app directory, app/assets/stylesheets/rspec_api_documentation/styles.css.  This allows users to override the default styles if so desired.
    
    The styles are read from the stylesheet and embedded in the HTML, allowing the individual example html pages to be styled without the need for an external resource.

--- lib/rspec_api_documentation/views/html_index.rb
+++ lib/rspec_api_documentation/views/html_index.rb
@@ -6,6 +6,12 @@ module RspecApiDocumentation
         self.template_name = "rspec_api_documentation/html_index"
       end
 
+      def styles
+        app_styles_url = File.join(Dir.pwd, "app", "assets", "stylesheets","rspec_api_documentation", "styles.css")
+        gem_styles_url = File.join(File.dirname(__FILE__), "..", "assets", "stylesheets","rspec_api_documentation", "styles.css")
+        return File.read(app_styles_url) rescue File.read(gem_styles_url)
+      end
+
       def examples
         @index.examples.map { |example| HtmlExample.new(example, @configuration) }
       end
