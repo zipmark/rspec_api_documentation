5a71d288c3f8077e3d541b4198bbc89c223be859
Marnen Laibow-Koser <mlaibowkoser@rbmtechnologies.com>
2016-04-28 14:58:11 -0400

    Set up cURL output. [#275]

--- lib/rspec_api_documentation/views/slate_example.rb
+++ lib/rspec_api_documentation/views/slate_example.rb
@@ -5,6 +5,12 @@ module RspecApiDocumentation
         super
         self.template_name = "rspec_api_documentation/slate_example"
       end
+
+      def curl_with_linebreaks
+        requests.map {|request| request[:curl].lines }.flatten.map do |line|
+          line.rstrip.gsub("\t", '  ').gsub(' ', '&nbsp;').gsub('\\', '&#92;')
+        end.join "<br>"
+      end
     end
   end
 end
