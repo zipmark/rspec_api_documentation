b8250a27bd4ce066a86e34c3e31e882aa4754c90
Marnen Laibow-Koser <mlaibowkoser@rbmtechnologies.com>
2016-04-28 16:37:16 -0400

    Remove obsolete failing test. [#275]

--- spec/writers/slate_writer_spec.rb
+++ spec/writers/slate_writer_spec.rb
@@ -22,20 +22,5 @@ describe RspecApiDocumentation::Writers::SlateWriter do
       subject { writer.markup_example_class }
       it { is_expected.to be == RspecApiDocumentation::Views::SlateExample }
     end
-
-    describe "#write" do
-      before do
-        template_dir = File.join(configuration.template_path, "rspec_api_documentation")
-        FileUtils.mkdir_p(template_dir)
-        File.open(File.join(template_dir, "markdown_index.mustache"), "w+") { |f| f << "{{ mustache }}" }
-        FileUtils.mkdir_p(configuration.docs_dir)
-      end
-
-      it "should write the index" do
-        writer.write
-        index_file = File.join(configuration.docs_dir, "index.markdown")
-        expect(File.exists?(index_file)).to be_truthy
-      end
-    end
   end
 end
