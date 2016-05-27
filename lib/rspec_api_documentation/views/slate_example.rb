132e02eba497cb1f6802ed82d981e45c3d8f0401
Rob Pocklington <rpocklin@gmail.com>
2016-05-27 11:57:30 +1000

    Moved explanation up to top of endpoint documentatino and changed curl to use shell formatter in slate.

--- lib/rspec_api_documentation/views/slate_example.rb
+++ lib/rspec_api_documentation/views/slate_example.rb
@@ -6,12 +6,6 @@ module RspecApiDocumentation
         self.template_name = "rspec_api_documentation/slate_example"
       end
 
-      def curl_with_linebreaks
-        requests.map {|request| request[:curl].lines }.flatten.map do |line|
-          line.rstrip.gsub("\t", '  ').gsub(' ', '&nbsp;').gsub('\\', '&#92;')
-        end.join "<br>"
-      end
-
       def explanation_with_linebreaks
         explanation.gsub "\n", "<br>\n"
       end
@@ -21,13 +15,13 @@ module RspecApiDocumentation
 
           sections.each do |section|
             file.write "# #{section[:resource_name]}\n\n"
-          end
 
-          section[:examples].examples.sort_by!(&:description) unless configuration.keep_source_order
+            section[:examples].examples.sort_by!(&:description) unless configuration.keep_source_order
 
-          section[:examples].examples.each do |example|
-            markup_example = markup_example_class.new(example, configuration)
-            file.write markup_example.render
+            section[:examples].examples.each do |example|
+              markup_example = markup_example_class.new(example, configuration)
+              file.write markup_example.render
+            end
           end
         end
       end
