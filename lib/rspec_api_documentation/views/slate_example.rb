22285704de53ff7288c49a3a995ceda8bcd575b7
Rob Pocklington <rpocklin@gmail.com>
2016-06-02 08:17:53 +1000

    Removed redundant 'write' method from slate example.

--- lib/rspec_api_documentation/views/slate_example.rb
+++ lib/rspec_api_documentation/views/slate_example.rb
@@ -5,22 +5,6 @@ module RspecApiDocumentation
         super
         self.template_name = "rspec_api_documentation/slate_example"
       end
-
-      def write
-        File.open(configuration.docs_dir.join("#{FILENAME}.#{extension}"), 'w+') do |file|
-
-          sections.each do |section|
-            file.write "# #{section[:resource_name]}\n\n"
-
-            section[:examples].examples.sort_by!(&:description) unless configuration.keep_source_order
-
-            section[:examples].examples.each do |example|
-              markup_example = markup_example_class.new(example, configuration)
-              file.write markup_example.render
-            end
-          end
-        end
-      end
     end
   end
 end
