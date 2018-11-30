8f5059b4538a42d106cf39a6fa46fa56f4f45dc9
Aaditya Taparia <aadityataparia@gmail.com>
2018-11-30 18:18:36 +0900

    Remove specs

--- spec/open_api/info_spec.rb
+++ spec/open_api/info_spec.rb
@@ -5,14 +5,7 @@ describe RspecApiDocumentation::OpenApi::Info do
   subject { node }
 
   describe "default settings" do
-    class RspecApiDocumentation::OpenApi::Contact; end
-    class RspecApiDocumentation::OpenApi::License; end
-
     its(:title) { should == 'OpenAPI Specification' }
-    its(:description) { should == 'This is a sample server Petstore server.' }
-    its(:termsOfService) { should == 'http://open-api.io/terms/' }
-    its(:contact) { should be_a(RspecApiDocumentation::OpenApi::Contact) }
-    its(:license) { should be_a(RspecApiDocumentation::OpenApi::License) }
     its(:version) { should == '1.0.0' }
   end
 end
