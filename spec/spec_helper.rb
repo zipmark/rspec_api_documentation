83764a7bf3f198da2d1e6736d0a9bb7982dcf752
Benjamin Fleischer <github@benjaminfleischer.com>
2025-06-30 17:01:40 -0500

    chore: remove unneeded

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,21 +3,6 @@ require 'fakefs/spec_helpers'
 require 'rspec/its'
 require 'pry'
 
-RspecApiDocumentation.configure do |config|
-  config.response_body_formatter = lambda do |response_content_type, response_body|
-    if response_content_type&.include?('application/json')
-      return JSON.pretty_generate(JSON.parse(response_body))
-    elsif response_content_type&.include?('text') || response_content_type&.include?('txt')
-      # quote it for JSON Parser in documentation reader like APITOME
-      return "\"#{response_body}\""
-    else
-      return '[binary data]'
-    end
-  rescue JSON::ParserError
-    '[binary data]'
-  end
-end
-
 RSpec.configure do |config|
   config.before(:all) do
     if self.class.metadata[:api_doc_dsl] || self.respond_to?(:app)
